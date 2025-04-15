0143ea95a01fd3254280968e40e8d4177db044cd
yousl902 <younes.sleibi2000@gmail.com>
2025-04-15 22:16:00 +0200

    getters of the authProvider based on the type of component (server, client). For now, noAuth if NEXT_PUBLIC_USE_AUTH (which should be added to .env) is not supabase

---
+++ lib/auth/factory/getServerProvider.ts
@@ -0,0 +1,7 @@
+import { AuthProvider } from '../types';
+import { noAuth } from '../strategies/noAuth';
+import { supabaseServerAuth } from '../strategies/supabaseServerAuth';
+
+export function getClientAuthProvider(): AuthProvider {
+  return process.env.NEXT_PUBLIC_USE_AUTH === 'supabase' ? supabaseServerAuth : noAuth;
+}
