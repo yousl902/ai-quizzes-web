a062208b312cae631aa2a5ae011818073b1ad105
Joar-E <joermi02@gmail.com>
2025-04-16 12:15:47 +0200

    Added a dropdown component

---
+++ app/quizMenu.tsx
@@ -0,0 +1,14 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+
+export default function QuizMeny(){
+    return (<div>
+        
+    </div>);
+}
