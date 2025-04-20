a4936b8a540e30d63b4060775ee5e8c368688b9f
yousl902 <younes.sleibi2000@gmail.com>
2025-04-20 17:18:29 +0200

    deleted exampleroute, not needed anymore

--- app/api/exampleroute/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-// Here we define the routes that will be used in the pages and the components to send requests
-// to use this in the components, we can use the route /api/exampleroute (path to this file)
