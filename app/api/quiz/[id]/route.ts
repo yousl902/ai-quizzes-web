0eb9563f82322ab818d53467c1224d52386704d7
yousl902 <younes.sleibi2000@gmail.com>
2025-04-21 12:49:29 +0200

    changed two things in this endpoint
    It will now return quizzes with thier questions and alternatives. It will also check if there exists a relation between the user fetching the quiz and the quiz itself.

--- app/api/quiz/[id]/route.ts
+++ app/api/quiz/[id]/route.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { AuthProvider } from "@/lib/auth/types";
 import { getServerAuthProvider } from "@/lib/auth/factory/getServerProvider";
-import { Quiz } from "@prisma/client";
+import { Quiz, UserQuiz } from "@prisma/client";
 
 export async function GET(
     _req: NextRequest,
@@ -15,15 +15,34 @@ export async function GET(
         return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
     }
 
+    const userId: string = user.id;
     const routeParams: { id: string } = await params;
     const quizId: string = routeParams.id;
 
     const quiz: Quiz | null = await prisma.quiz.findUnique({
         where: { id: quizId },
+        include: {
+            questions: {
+                include: {
+                    alternatives: true,
+                },
+            },
+        },
     });
 
     if (!quiz) {
         return NextResponse.json({ error: "Quiz not found" }, { status: 404 });
+    }
+
+    const userQuizRelation: UserQuiz | null = await prisma.userQuiz.findFirst({
+        where: {
+            user_id: userId,
+            quiz_id: quizId,
+        },
+    });
+
+    if (!userQuizRelation) {
+        return NextResponse.json({ error: "No relation exists between the user and the quiz" }, { status: 404 });
     }
 
     return NextResponse.json(quiz, { status: 200 });
