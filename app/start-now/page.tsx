8c2833c757326bb0669496057e7fd52faa2e5259
yousl902 <younes.sleibi2000@gmail.com>
2025-04-22 10:45:31 +0200

    change styling of temp loading div

--- app/start-now/page.tsx
+++ app/start-now/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
   }, []);
 
   if (user === undefined) {
-    return <div>Loading...</div>;
+    return <div className="flex items-center justify-center min-h-screen"> Loading ...</div>;
   }
 
   return (
