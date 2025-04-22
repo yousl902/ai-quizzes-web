5eed8321054a220aeb10d3d8aa8391f46cf15fee
yousl902 <younes.sleibi2000@gmail.com>
2025-04-22 23:41:03 +0200

    updated the global config file with needed info

--- siteConfig.ts
+++ siteConfig.ts
@@ -1,6 +1,11 @@
 const siteInfo = {
     title: "HögskoleprovetQuizes",
     logo: "/Mind_Swarm_Logo.png", // path relative to /public
+    errorGif: {
+        src: "https://media.giphy.com/media/1NQ7m0gqsah1XS4vG1/giphy.gif",
+        alt: "Animated bee",
+    },
+    colors: {},
     info: {
         contact: {
             email: "info@studycomb.se",
