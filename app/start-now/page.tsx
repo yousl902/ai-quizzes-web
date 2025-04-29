a51cdd23138d6a010ba74800b0e33f292aee328f
yousl902 <younes.sleibi2000@gmail.com>
2025-04-29 12:46:28 +0200

    deleted logout button, should not be in this page

--- app/start-now/page.tsx
+++ app/start-now/page.tsx
@@ -1,19 +1,9 @@
 import QuizCard from "@/components/QuizCard";
-import { Button } from "@/components/ui/button";
-import { logout } from "../actions/auth";
 
-export default function HomePage() {
+export default function StartNow() {
   return (
     <div className="bg-gradient-to-br from-yellow-100 via-yellow-50 to-white min-h-screen flex flex-col items-center justify-center">
       <QuizCard />
-      <form>
-        <Button
-          formAction={logout}
-          className="bg-black text-white mt-5 hover:bg-gray-800"
-        >
-          Logout
-        </Button>
-      </form>
     </div>
   );
 }
