65d12b9991a7c7905fea221f3d639d29030caa1b
yousl902 <younes.sleibi2000@gmail.com>
2025-04-30 10:28:41 +0200

    change location of auth pages to change the route and updated imports

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -1,10 +1,10 @@
-import ResetPasswordForm from "@/components/ResetPasswordForm";
+import ForgotPasswordForm from "@/components/forms/ForgatPasswordForm";
 
-export default function ResetPassword() {
+export default function ForgotPassword() {
   return (
     <div className="page-container bg-gradient-to-br from-yellow-100 via-yellow-50 to-white">
       <main className="flex-1 flex flex-col items-center h-screen justify-center py-16 px-4">
-        <ResetPasswordForm />
+        <ForgotPasswordForm />
       </main>
     </div>
   );
