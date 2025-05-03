1ab1e46f2faf9dc5651e6d9f1137d47d9128624e
yousl902 <younes.sleibi2000@gmail.com>
2025-05-03 12:09:17 +0200

    dynamic route to go to a specific quiz by quizId

---
+++ app/[id]/page.tsx
@@ -0,0 +1,9 @@
+import QuizCard from "@/components/quizPage/QuizCard";
+
+export default function QuizPage() {
+  return (
+    <div className="bg-gradient-to-br from-yellow-100 via-yellow-50 to-white min-h-screen flex flex-col items-center justify-center">
+      <QuizCard />
+    </div>
+  );
+}
