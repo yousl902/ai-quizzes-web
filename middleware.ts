c0838a2fff6f4e90d402ecd66766857f6a773ca3
yousl902 <younes.sleibi2000@gmail.com>
2025-05-09 20:05:36 +0200

    feat: integrate next-intl middleware for internationalization support

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,15 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/lib/supabase/middleware'
+import createMiddleware from "next-intl/middleware";
+import { type NextRequest } from "next/server";
+import { updateSession } from "@/lib/supabase/middleware";
+import { routing } from "./i18n/routing";
+
+const handleI18nRouting = createMiddleware(routing);
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  const response = handleI18nRouting(request);
+
+  // A `response` can now be passed here
+  return await updateSession(request, response);
 }
 
 export const config = {
@@ -12,8 +19,10 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - api
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    // '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
-}
+};
