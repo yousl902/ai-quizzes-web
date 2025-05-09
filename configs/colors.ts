c236c28575b4e096876653b3525f0589885a245a
yousl902 <younes.sleibi2000@gmail.com>
2025-05-09 20:02:54 +0200

    add colors configuration for color scale,  just a placeholder until we get the color config ready

---
+++ configs/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  "colorScale": [
+    { "max": 4, "color": "hsl(15, 100%, 55%)" },
+    { "max": 14, "color": "hsl(30, 100%, 50%)" },
+    { "max": Infinity, "color": "hsl(45, 100%, 50%)" }
+  ]
+}
+
