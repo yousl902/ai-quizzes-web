a6bf7b26d4e7b9d1bf54fab00b5328e106a7a5e3
yousl902 <younes.sleibi2000@gmail.com>
2025-05-14 10:53:57 +0200

    feat: update assets and localization for Database Technology branding and added colors.json

--- configs/colors.ts
+++
@@ -1,8 +0,0 @@
-export const colors = {
-  "colorScale": [
-    { "max": 4, "color": "hsl(15, 100%, 55%)" },
-    { "max": 14, "color": "hsl(30, 100%, 50%)" },
-    { "max": Infinity, "color": "hsl(45, 100%, 50%)" }
-  ]
-}
-
