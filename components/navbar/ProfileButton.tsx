d3fcec34529a58a75c99042c915ef01d29252417
yousl902 <younes.sleibi2000@gmail.com>
2025-05-20 22:01:35 +0200

    refactor: simplify ProfileButton component by removing unused code and integrating ProfileSection for user details

--- components/navbar/ProfileButton.tsx
+++ components/navbar/ProfileButton.tsx
@@ -1,27 +1,24 @@
-"use client";
-
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { logout } from "@/app/actions/auth";
 import { Button } from "@/components/ui/button";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { ProfileChart } from "@/components/navbar/ProfileChart";
-import { useEffect, useState } from "react";
-import { useTranslations } from "next-intl";
+import { ProfileSection } from "@/components/navbar/ProfileSection";
 
 interface ProfileButtonProps {
   email: string;
   firstName?: string;
   lastName?: string;
+  userId: string;
 }
 
-export function ProfileButton({
+export async function ProfileButton({
   firstName,
   lastName,
   email,
+  userId,
 }: ProfileButtonProps) {
   const fullName = `${firstName || ""} ${lastName || ""}`.trim();
   const getInitials = (name: string) =>
@@ -30,28 +27,6 @@ export function ProfileButton({
       .map((word) => word[0])
       .join("")
       .toUpperCase();
-
-  const t = useTranslations("navbar");
-  const [quizResults, setQuizResults] = useState<
-    { quizId: string; score: number; title: string }[]
-  >([]);
-
-  useEffect(() => {
-    const lastTenResults = async () => {
-      const res = await fetch(`/api/quiz/last-results?numberOfQuizzes=10`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (!res.ok) {
-        throw new Error("Failed to fetch quiz results");
-      }
-      const quizResults = await res.json();
-      setQuizResults(quizResults);
-    };
-    lastTenResults();
-  }, []);
 
   return (
     <Popover>
@@ -65,29 +40,7 @@ export function ProfileButton({
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-80 backdrop-blur-sm bg-white/90 border border-orange-200">
-        <div className="space-y-4">
-          <div className="space-y-2">
-            <h4 className="font-medium leading-none">{fullName}</h4>
-            <p className="text-sm text-muted-foreground">{email}</p>
-          </div>
-
-          {/* Diagram */}
-          <div className="pt-2">
-            <ProfileChart quizResults={quizResults} />
-          </div>
-
-          {/* Log-out */}
-          <form>
-            <div className="flex justify-center">
-              <Button
-                formAction={logout}
-                className="mt-5 bg-btn-logout text-white hover:bg-btn-logout/90 hover:scale-105 transition-colors duration-200"
-              >
-                {t("logout")}
-              </Button>
-            </div>
-          </form>
-        </div>
+        <ProfileSection firstName={firstName} lastName={lastName} email={email} userId={userId} />
       </PopoverContent>
     </Popover>
   );
